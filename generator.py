c8ed03668be3b8aaf124ed61d39356ad71c11280
Darius <68792754+Darius730@users.noreply.github.com>
2020-08-03 22:08:53 +0300

    Delete generator.py

--- generator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def password():
-    for i in range(6):
-        yield random.randint(1213231000, 9994345555600)
-        yield random.randint (100000, 9000000)
-
-for random_password in password():
-    print("The generated password is: %d" % (random_password))
